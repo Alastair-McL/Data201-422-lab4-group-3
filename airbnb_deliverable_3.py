e6c512b7f7608b1778fd54da4029c7d7dfb5fea7
Alastair-McL <alastair.mclauchlan0@gmail.com>
2026-08-06 10:14:12 +1200

    Add airbnb_deliverable_3.py

---
+++ airbnb_deliverable_3.py
@@ -0,0 +1,10 @@
+'''Data201/422 
+AirBnB deliverable 3
+Alastair McLauchlan
+Sophie Mcnee
+Darrel Susan Binu
+Chinnu Rappai
+'''
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
